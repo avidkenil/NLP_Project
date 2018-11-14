84f7e59f30d09d6efe7569763f23a728da406b14
dmesquita <d.mesquita285@gmail.com>
2018-11-14 15:05:10 -0500

    make cnn encoder

--- seq2seq_nlp/models/encoder_network.py
+++ seq2seq_nlp/models/encoder_network.py
@@ -1,22 +1,21 @@
+# Notation in the comments throughout:
+# B: batch size
+# T: length of sequence
+# E: embedding size
+# H: hidden size
+# D: num_directions
+# L: num_layers
+
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-
 
 class RNNEncoder(nn.Module):
 
     def __init__(self, kind, vocab_size, embed_size, hidden_size, num_layers,
                  bidirectional, return_type='full_last_layer',
-                 p_dropout_hidden=0):
+                 prob_dropout_hidden=0):
         '''
-        Notation:
-            B: batch size
-            T: length of sequence
-            E: embedding size
-            H: hidden size
-            D: num_directions
-            L: num_layers
-
         Arg in:
             kind (str): one of: 'rnn', 'gru', 'lstm'
             bidirectional (bool)
@@ -42,7 +41,7 @@ class RNNEncoder(nn.Module):
         self.table_lookup = nn.Embedding(vocab_size, embed_size, padding_idx=0)
         self.rnn = self.rnn(embed_size, hidden_size, num_layers,
                             bidirectional=bidirectional, batch_first=True,
-                            dropout=p_dropout_hidden)
+                            dropout=prob_dropout_hidden)
 
     def init_state(self, batch_size):
         return torch.randn(self.num_layers * self.num_directions, batch_size,
@@ -62,14 +61,137 @@ class RNNEncoder(nn.Module):
         return out
 
 
+class CNNEncoder(nn.Module):
+
+    def __init__(self, vocab_size, embed_size, hidden_size, kernel_sizes, num_layers, prob_dropout_hidden=0, dropout_type='1d'):
+        super().__init__()
+
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.kernel_sizes = kernel_sizes
+
+        self.table_lookup = nn.Embedding(vocab_size, embed_size, padding_idx=0)
+
+        dropout = nn.Dropout if dropout_type == '1d' else nn.Dropout2d
+        # TODO: implement dropout2d
+        dropout = nn.Dropout
+        cnn_sequences = []
+        for kernel_size in kernel_sizes:
+            cnn_sequence = []
+            for layer in range(num_layers):
+                in_channels = embed_size if layer == 0 else hidden_size
+                cnn_sequence.extend(
+                    [nn.Conv1d(in_channels, hidden_size, kernel_size,
+                               padding=kernel_size // 2),
+                     nn.ReLU(),
+                     dropout(prob_dropout_hidden)])
+            cnn_sequences.append(nn.Sequential(*cnn_sequence))
+
+        self.cnn_sequences = nn.ModuleList(cnn_sequences)
+
+    def forward(self, x, x_lens):
+        embed = self.table_lookup(x).transpose(1, 2)  # B, H, T
+        outputs = []
+        for ix, cnn in enumerate(self.cnn_sequences):
+            out = cnn(embed).transpose(1,2)  # (B, T, H)
+            to_add = self.num_layers if self.kernel_sizes[ix] % 2 == 0 else 0
+            assert(out.size() == (x.size(0), x.size(1) +
+                                  to_add, self.hidden_size))
+            outputs.append(out)
+
+        if self.num_layers == 1:
+            # max-pooling
+            for ix, ele in enumerate(outputs):
+                outputs[ix] = ele.max(dim=1)[0]
+        else:
+            # avg-pooling
+            for ix, ele in enumerate(outputs):
+                outputs[ix] = ele.sum(dim=1) / x_lens  # (B, H)
+
+        out = torch.cat(outputs, dim=1)
+        assert(out.size() == (x.size(0),
+                              len(self.kernel_sizes) * self.hidden_size))
+        return out
+
+
 if __name__ == '__main__':
 
-    print('tests')
-    x_lens = torch.FloatTensor([[5],[4],[4],[2]])
+    x_lens = torch.FloatTensor([[5], [4], [4], [2]])
     V, B, T, H = 5000, 4, 5, 500
 
     x = torch.randint(0, V, size=(B, T)).long()
 
-    gru = RNNEncoder('gru', V, 500, H, 3, True, p_dropout_hidden=.5)
+    print('#### Tests rnn')
+    gru = RNNEncoder('gru', V, 500, H, 3, True, prob_dropout_hidden=.5)
     out = gru(x, x_lens)
     assert(out.size() == (B, T, 2 * H))
+    print('\ttests passed')
+    print()
+    print('#### Tests cnn')
+    L = 1
+    kernel_sizes = [2]
+    print(f'\tB={B}; H={H}, T={T}, L={L}, kernel_sizes={kernel_sizes}')
+    cnn_encoder = CNNEncoder(V, 500, H, num_layers=1, kernel_sizes=kernel_sizes, prob_dropout_hidden=.5, dropout_type='1d')
+    out = cnn_encoder(x, x_lens)
+    assert(out.size() == (B, len(kernel_sizes) * H))
+    print('\ttests passed')
+    print()
+
+
+    print('#### Tests cnn')
+    L = 3
+    kernel_sizes = [2]
+    print(f'\tB={B}; H={H}, T={T}, L={L}, kernel_sizes={kernel_sizes}')
+    cnn_encoder = CNNEncoder(V, 500, H, num_layers=L, kernel_sizes=kernel_sizes, prob_dropout_hidden=.5, dropout_type='1d')
+    out = cnn_encoder(x, x_lens)
+    assert(out.size() == (B, len(kernel_sizes) * H))
+    print('\ttests passed')
+    print()
+
+    print('#### Tests cnn')
+    L = 3
+    kernel_sizes = [2,3,4,5]
+    print(f'\tB={B}; H={H}, T={T}, L={L}, kernel_sizes={kernel_sizes}')
+    cnn_encoder = CNNEncoder(V, 500, H, num_layers=L, kernel_sizes=kernel_sizes, prob_dropout_hidden=.5, dropout_type='1d')
+    out = cnn_encoder(x, x_lens)
+    assert(out.size() == (B, len(kernel_sizes) * H))
+    print('\ttests passed')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
