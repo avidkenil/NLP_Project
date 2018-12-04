69287f488f9b131d6ce0ec5577099bf516b7960b
Mihir Rana <ranamihir@gmail.com>
2018-12-04 17:31:44 -0500

    Small bug fixes.

--- seq2seq_nlp/utils.py
+++ seq2seq_nlp/utils.py
@@ -5,6 +5,7 @@ import logging
 import time
 import sys
 import pickle
+from types import ModuleType
 from pprint import pformat
 
 import torch
@@ -75,7 +76,7 @@ def train(encoder, decoder, dataloader, criterion, optimizer, epoch, max_len_tar
 
         # # init the decoder outputs with zeros and then fill them up with the values
         # decoder_outputs = torch.zeros(source.size(0), max_len_target, decoder.vocab_size).to(device)
-        encoder_output, encoder_hidden = encoder(source,source_lens)
+        encoder_output, encoder_hidden = encoder(source, source_lens)
         # doing complete teacher forcing first and then will add the probability based teacher forcing
         if decoder.num_layers == 1:
             if encoder.num_directions == 1:
@@ -146,7 +147,8 @@ def test(encoder, decoder, dataloader, criterion, device):
     return loss_test, torch.cat(output_hist, dim=0), torch.cat(y_hist, dim=0)
 
 def print_config(vars_dict):
-    vars_dict = {key: value for key, value in vars_dict.items() if key == key.upper()}
+    vars_dict = {key: value for key, value in vars_dict.items() if key == key.upper() \
+                 and not isinstance(value, ModuleType)}
     logging.info(pformat(vars_dict))
 
 def save_plot(project_dir, plots_dir, fig, filename):
@@ -254,7 +256,6 @@ def save_checkpoint(encoder, decoder, optimizer, train_loss_history, val_loss_hi
 
     source_dataset = os.path.splitext(args.source_dataset)[0]
     target_dataset = os.path.splitext(args.target_dataset)[0]
-
     params = [source_dataset, target_dataset, args.source_vocab, args.target_vocab, \
               args.max_len_source, args.max_len_target, args.encoder, args.num_directions, \
               args.encoder_num_layers, args.decoder_num_layers, args.encoder_emb_size, \
@@ -269,9 +270,8 @@ def save_checkpoint(encoder, decoder, optimizer, train_loss_history, val_loss_hi
     logging.info('Done.')
 
 def remove_checkpoint(args, project_dir, checkpoints_dir, epoch):
-    source_dataset = os.path.splitext(source_dataset)[0]
-    target_dataset = os.path.splitext(target_dataset)[0]
-
+    source_dataset = os.path.splitext(args.source_dataset)[0]
+    target_dataset = os.path.splitext(args.target_dataset)[0]
     params = [source_dataset, target_dataset, args.source_vocab, args.target_vocab, \
               args.max_len_source, args.max_len_target, args.encoder, args.num_directions, \
               args.encoder_num_layers, args.decoder_num_layers, args.encoder_emb_size, \
@@ -326,6 +326,8 @@ def load_checkpoint(encoder, decoder, optimizer, checkpoint_file, project_dir, c
             train_bleu_history, val_bleu_history, epoch_trained
 
 def save_model(model, model_name, epoch, args, project_dir, checkpoints_dir):
+    source_dataset = os.path.splitext(args.source_dataset)[0]
+    target_dataset = os.path.splitext(args.target_dataset)[0]
     params = [source_dataset, target_dataset, args.source_vocab, args.target_vocab, \
               args.max_len_source, args.max_len_target, args.encoder, args.num_directions, \
               args.encoder_num_layers, args.decoder_num_layers, args.encoder_emb_size, \
